16773cc97c243cfc3ce02f321ca1328e1e7b07a8
Helen Roehner <hroehner@ad.unc.edu>
2023-03-02 08:55:02 -0500

    CL02 (2)

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,8 +1,8 @@
 """Example function for unit tests"""
 
-def sum(xs: list[float]) -> float:
+def new_sum(xs: list[float]) -> float:
     """return sum of all elements in xs"""
     sum_total: float = 0.0
     for idx in xs: 
-        sum_total += idx 
+        sum_total += idx
     return sum_total 
