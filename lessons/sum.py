7c4eb9138206275daa9b3f85fc369fbd053478b8
Helen Roehner <hroehner@ad.unc.edu>
2023-03-02 09:05:53 -0500

    CL02 (3)

--- lessons/sum.py
+++ lessons/sum.py
@@ -3,6 +3,6 @@
 def new_sum(xs: list[float]) -> float:
     """return sum of all elements in xs"""
     sum_total: float = 0.0
-    for idx in xs: 
-        sum_total += idx
-    return sum_total 
+    for item in xs: 
+        sum_total += item
+    return sum_total
