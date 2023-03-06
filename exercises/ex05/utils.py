ec321f0f1f77ae3c138fb2e8b86d4be9fcc26f3b
Helen Roehner <hroehner@ad.unc.edu>
2023-03-06 15:03:32 -0500

    utils ex05 (2)

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,6 +2,7 @@
 
 __author__ = "730472095"
 
+
 def only_evens(num_list: list[int]) -> list[int]:
     """Given a list of integers return the even elements."""
     even_list: list[int] = []
@@ -10,14 +11,16 @@ def only_evens(num_list: list[int]) -> list[int]:
             even_list.append(num)
     return even_list 
 
+
 def concat(list_one: list[int], list_two: list[int]) -> list[int]: 
     """A new list containing elements of the first list followed by all the elements in the second list."""
-    new_list : list[int] = []
+    new_list: list[int] = []
     for item in list_one: 
         new_list.append(item)
     for item in list_two: 
         new_list.append(item)
     return new_list
+
 
 def sub(a_list: list[int], start: int, end: int): 
     """Creating a new list which is a subset of a given list between the start and end index."""
@@ -25,7 +28,7 @@ def sub(a_list: list[int], start: int, end: int):
     if start < 0: 
         start = 0 
     if end > len(a_list): 
-        end = len(a_list) - 1
+        end = len(a_list) 
     if len(a_list) == 0: 
         return []
     if start >= len(a_list): 
